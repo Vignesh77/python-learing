092b2922c7fd8d9059ab038e3acef586d6865bd7
Vignesh77 <vignesh.nanjundan@vvdntech.in>
2017-05-16 17:06:02 +0530

    Add files via upload

---
+++ num_1to10.py
@@ -0,0 +1,13 @@
+"""
+@file       :num_1to10.py
+@brief      :Print the number from 1 to 10 using while loop.
+@author     :vignesh(vignesh.nanjundan@vvdntech.in)
+@copyright  :vvdn technology,chennai.
+
+"""
+number = 1
+num_range = input("Enter the range...\n")
+while number <= num_range :
+    print number
+    number = number + 1
+
