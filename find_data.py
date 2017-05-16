80bdd4b2ae8d1b03beb46c1236da9e17669f12c3
Vignesh77 <vignesh.nanjundan@vvdntech.in>
2017-05-16 18:36:37 +0530

    Update find_data.py

--- find_data.py
+++ find_data.py
@@ -2,7 +2,6 @@
 @file       :find_data.py
 @brief      :Find the given input as string|integer|float using type() and str|int|float|bool
 @author     :vignesh(vignesh.nanjundan@vvdntech.in)
-@copyright  :vvdn technology,chennai.
 
 """
 input_data = input("Enter the input data...\n")
