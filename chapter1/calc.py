af6238d4575828ab83d29036c12b725517ecf5a2
vigneshn <vigneshece7766@gmail.com>
2017-05-16 14:43:29 +0530

    basic_calc

---
+++ chapter1/calc.py
@@ -0,0 +1,16 @@
+"""
+# @file         :calc.py
+# @brief        : Write a program for simple arithmetic operations like
+#               add,subtract, multiplication, division.
+#@author        :vignesh(vignesh.nanjundan@vvdntech.in)
+#@copyright     :vvdn technology,chennai.
+
+"""
+number1 = (int)(input("Enter the first Number...\n"))
+number2 = (int)(input("Enter the second Number...\n"))
+
+print " add = ", number1 + number2
+print " sub = ", number1 - number2
+print " mul = ", number1 * number2
+print " div = ", number1 // number2
+
