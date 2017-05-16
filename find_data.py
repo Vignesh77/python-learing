b24a93d7fdd6b317474f97c20ad4fe7a32e2b57c
Vignesh77 <vignesh.nanjundan@vvdntech.in>
2017-05-16 15:10:33 +0530

    Add files via upload

---
+++ find_data.py
@@ -0,0 +1,12 @@
+"""
+@file       :find_data.py
+@brief      :Find the given input as string|integer|float using type() and str|int|float|bool
+@author     :vignesh(vignesh.nanjundan@vvdntech.in)
+@copyright  :vvdn technology,chennai.
+
+"""
+input_data = input("Enter the input data...\n")
+print type(input_data)
+input_data = raw_input("Enter the input data..\n")
+print type(input_data)
+
