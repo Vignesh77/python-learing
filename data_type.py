59a4c36dbf399ffadafd1422b34b96c39d4bd0a7
Vignesh77 <vignesh.nanjundan@vvdntech.in>
2017-05-16 18:36:06 +0530

    Update data_type.py

--- data_type.py
+++ data_type.py
@@ -2,7 +2,6 @@
 @file       :data_type.py
 @brief      :Get the input from user and convert it into different data types and print it.
 @author     :vignesh(vignesh.nanjundan@vvdntech.in)
-@copyright  :vvdn technology,chennai.
 
 """
 input_data = input("Enter the input data...\n")
