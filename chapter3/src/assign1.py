b4fc5c4802b71e6728c755175917ea7a784edb0c
vigneshn <vigneshece7766@gmail.com>
2017-05-23 17:24:43 +0530

    assign1.py

---
+++ chapter3/src/assign1.py
@@ -0,0 +1,12 @@
+"""
+@file       :assign.py
+@brief      :Check the 'n' and 'l' is available or not in 'gnu'
+@author     :vignesh(vignesh.nanjundan@vvdntech.in)
+
+"""
+if 'n' in 'gnu' :
+    print " 'n' is available"
+elif 'l' in 'gnu' :
+    print " 'l' is available"
+else :
+    print "Not available"
